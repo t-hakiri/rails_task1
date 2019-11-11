730f63d397ba36f0f5151ee6de684ed58e52e6e4
takuya hakiri <tyapi4@yahoo.co.jp>
2019-11-11 18:08:13 +0900

    create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,51 @@
 class TweetsController < ApplicationController
-  def toppage
-  end
+	def toppage
+ 	end
 
-  def index
-  end
+  	def index
+  		@tweets = Tweet.all
+  	end
+
+  	def new
+  		@tweet = Tweet.new
+  	end
+	
+	def create
+  		@tweet = Tweet.new(tweet_params)
+  		if params[:back]
+  			render :new
+  		else
+  			if @tweet.save
+  				redirect_to tweets_path, notice: "tweetしました。"
+  			else
+  				render :new
+  			end
+  		end
+  	end
+
+  	def edit
+  	end
+
+  	def update
+  		if @tweet.update(tweet_params)
+  			redirect_to tweet_path, notice: "編集しました。"
+  		end
+  	end
+
+  	def destroy
+  		@tweet.destroy
+  		redirect_to tweets_path, notice: "削除しました。"
+  	end
+
+ 	def show
+  	end
+
+ 	def confirm
+ 	end
+
+ private 
+  	def tweet_params
+  		params.require(:tweet).permit(:content)
+  	end
+
 end
