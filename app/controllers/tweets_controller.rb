8c1aef2516fc6f248a7db8831d87d6069dc65d4d
takuya hakiri <tyapi4@yahoo.co.jp>
2019-11-12 15:33:16 +0900

    修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,51 +1,51 @@
-class TweetsController < ApplicationController
-	before_action :set_tweet, only: [:edit, :update, :destroy]
+uclass TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :update, :destroy]
 
-	def toppage
- 	end
+  def toppage
+  end
 
   def index
-		@tweets = Tweet.all
- 	end
-
- 	def new
-  	@tweet = Tweet.new
+    @tweets = Tweet.all
   end
-	
-	def create
-  	@tweet = Tweet.new(tweet_params)
-  	if params[:back]
-  		render :new
-  	else
-  		if @tweet.save
-  			redirect_to tweets_path, notice: "tweetしました。"
-  		else
-  			render :new
-  		end
-  	end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if params[:back]
+      render :new
+    else
+    if @tweet.save
+      redirect_to tweets_path, notice: "tweetしました。"
+    else
+     render :new
+    end
+    end
   end
 
   def edit
-  	@tweet = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:id])
   end
 
   def update
-  	if @tweet.update(tweet_params)
-  		redirect_to tweets_path, notice: "編集しました。"
-  	end
-  	end
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "編集しました。"
+    end
+  end
 
-	def destroy
-  	@tweet.destroy
-  	redirect_to tweets_path, notice: "削除しました。"
-	end
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "削除しました。"
+  end
 
- 	def confirm
- 		@tweet = Tweet.new(tweet_params)
- 		render :new if @tweet.invalid?
- 	end
+  def confirm
+    @tweet = Tweet.new(tweet_params)
+    render :new if @tweet.invalid?
+  end
 
- 	private 
+  private 
   def tweet_params
     params.require(:tweet).permit(:content)
   end
